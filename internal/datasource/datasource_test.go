62c8710bf9f141e407d7e7c48c6f5dff14add80e
shaninalex <shanin.x@gmail.com>
2023-09-16 12:59:01 +0300

    #4 datasource initialization tests

---
+++ internal/datasource/datasource_test.go
@@ -0,0 +1,22 @@
+package datasource
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestDataSourceInitialization(t *testing.T) {
+	datasource := InitializeDatasource(
+		os.Getenv("GURUFOCUS_API_KEY"),
+		"demo",
+	)
+
+	if datasource.Gurufocus == nil {
+		t.Error("Failed to initialize GuruFocus subservice")
+	}
+
+	if datasource.Alphavantage == nil {
+		t.Error("Failed to initialize Alphavantage subservice")
+	}
+}
