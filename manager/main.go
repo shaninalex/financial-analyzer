ff728ede825a7b6384de771191d9bca2338e9ecf
shaninalex <shanin.x@gmail.com>
2023-10-01 20:20:47 +0300

    #14 setup websocket

---
+++ manager/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		// connect only through trusted servers
+		return true
+	},
+}
+
+func main() {
+	http.HandleFunc("/", echo)
+	log.Println("Server is running on port 8003")
+	http.ListenAndServe(":8003", nil)
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	connection, _ := upgrader.Upgrade(w, r, nil)
+	defer connection.Close()
+	log.Println(r.Cookies())
+	for {
+		mt, message, err := connection.ReadMessage()
+
+		if err != nil || mt == websocket.CloseMessage {
+			break
+		}
+
+		connection.WriteMessage(websocket.TextMessage, message)
+		go messageHandler(message)
+	}
+}
+
+func messageHandler(message []byte) {
+	log.Println(string(message))
+}
