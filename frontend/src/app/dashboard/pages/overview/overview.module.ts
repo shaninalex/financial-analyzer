bfc8b6127255772676a0c87d81c41cc6d57542bd
shaninalex <shanin.x@gmail.com>
2023-12-16 15:08:23 +0200

    fix: remove icons module

--- frontend/src/app/dashboard/pages/overview/overview.module.ts
+++ frontend/src/app/dashboard/pages/overview/overview.module.ts
@@ -3,12 +3,7 @@ import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UiModule } from '../../ui/ui.module';
-import { NgIconsModule } from '@ng-icons/core';
-import { 
-    iconoirArrowTr,
-    iconoirSaveFloppyDisk,
-    iconoirPlus,
-} from '@ng-icons/iconoir';
+
 import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
@@ -23,11 +18,6 @@ const routes: Routes = [
         CommonModule,
         UiModule,
         ReactiveFormsModule,
-        NgIconsModule.withIcons({ 
-            iconoirArrowTr,
-            iconoirSaveFloppyDisk,
-            iconoirPlus,
-        }),
         RouterModule.forChild(routes)
     ]
 })
