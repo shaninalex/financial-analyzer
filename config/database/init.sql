bb7d4ad86e3e3117ce74bb030e0e93b7ec31bc4b
shaninalex <shanin.x@gmail.com>
2024-02-11 16:46:37 +0200

    fix(db): define simple reports table

--- config/database/init.sql
+++ config/database/init.sql
@@ -1,2 +1,35 @@
 CREATE SCHEMA main;
 CREATE SCHEMA kratos;
+
+
+-- This is main application schema.
+-- I do not wnat to mess arround with ORM's because the schema of this 
+-- application is preaty simple, and I can use simple raw sql queries
+
+-- Creating the 'issues' table
+-- some reports or other errors will be stored here
+CREATE TABLE main.issues (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(100),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- This table contain all user generated reports
+-- I intentionaly does not provide default `id` since it should be provided 
+-- outside of db.
+CREATE TABLE main.reports (
+    id UUID PRIMARY KEY,  
+    user_id UUID NOT NULL,
+    ticker VARCHAR(255) NOT NULL,
+    link VARCHAR(255),
+    email_send VARCHAR(255),
+    email_recepient VARCHAR(255),
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    success BOOLEAN,
+    issue_id UUID REFERENCES main.issues(id)
+);
+
+-- Creating an index on the 'user_id' column
+CREATE INDEX ON main.reports (user_id);
+
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/goccy/go-json v0.10.2
 	github.com/google/uuid v1.5.0
 	github.com/gorilla/websocket v1.5.1
+	github.com/lib/pq v1.10.9
 	github.com/ory/kratos-client-go v1.0.0
 	github.com/rabbitmq/amqp091-go v1.9.0
 )
