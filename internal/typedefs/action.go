1dfd408d9fd543fda9fb1e739ed4a68bbd6a58f5
shaninalex <shanin.x@gmail.com>
2024-03-16 17:26:01 +0200

    fix(router): Changing action types #75

--- internal/typedefs/action.go
+++ internal/typedefs/action.go
@@ -1,5 +1,6 @@
 package typedefs
 
+// Deprecated: Use ActionType
 type TickerActionType string
 
 var (
@@ -8,7 +9,23 @@ var (
 	TickerActionTypeReport  TickerActionType = "report"
 )
 
+// Deprecated: Use ActionType
 type ITickerAction struct {
 	Ticker string           `json:"ticker"`
 	Action TickerActionType `json:"action"`
+}
+
+type ActionType string
+
+var (
+	ActionTypeReport      ActionType = "report"
+	ActionTypeGeneratePdf ActionType = "generate_pdf"
+	ActionTypeSendEmail   ActionType = "send_email"
+)
+
+type Action struct {
+	Ticker         string     `json:"ticker"`
+	Action         ActionType `json:"action"`
+	RequestId      string     `json:"request_id"`
+	EmailRecepient string     `json:"email_recepient"`
 }
